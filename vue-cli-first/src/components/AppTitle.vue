73c9527c84efcbf1b8eae9c8bb645b85af2eff34
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-09-21 20:00:41 +0200

    Creazione del componente AppTitle

---
+++ vue-cli-first/src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle'
+}
+</script>
+
+<style scoped lang="scss">
+    h1{
+        color: yellowgreen;
+        font-size: 2.5rem;
+    }
+</style>
